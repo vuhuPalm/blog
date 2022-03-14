29410a2b221db7567eede6bfffb7b8f804d61de5
vuhuPalm <dpalmer1701@gmail.com>
2022-03-14 08:55:38 -0700

    intial commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const articleController = require('../controllers/articleController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/article/add', articleController.renderAddForm);
+router.post('/article/add', articleController.addArticle);
 
+router.get('/article/:articleId', articleController.displayArticle);
 module.exports = router;
